af5485d0d3eabb985d588e8f5a9d6ea1ba08b1d0
TSP-Main <sardarnawaz122@gmail.com>
2025-05-03 15:26:40 +0500

    updated seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
- use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -11,32 +11,36 @@ class UserSeeder extends Seeder
     public function run()
     {
         // Create admin user
-        $admin = User::create([
+        User::create([
+            'name' => 'Admin User',
             'username' => 'admin',
-            'name' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('admin1234'), 
+            'password' => Hash::make('admin1234'),
             'role' => 'admin',
-            'is_verified' => true
+            'is_verified' => true,
+            'email_verified_at' => now(),
         ]);
- 
+
         // Create author user
-        $author = User::create([
+        User::create([
             'name' => 'Author User',
             'username' => 'author',
             'email' => 'author@example.com',
             'password' => Hash::make('password'),
+            'role' => 'author',
+            'is_verified' => true,
             'email_verified_at' => now(),
         ]);
- 
+
         // Create editor user
-        $editor = User::create([
+        User::create([
             'name' => 'Editor User',
             'username' => 'editor',
             'email' => 'editor@example.com',
             'password' => Hash::make('password'),
+            'role' => 'editor',
+            'is_verified' => true,
             'email_verified_at' => now(),
         ]);
-      
     }
 }
