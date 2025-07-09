07e770f221aced0a88e3ae40595e73719d091f9f
TSP-Main <sardarnawaz122@gmail.com>
2025-07-09 23:13:30 +0500

    added route clearing cache and creating storage link

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 use App\Http\Controllers\Seo\ImageGeneratorController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+Route::get('/optimize-clear', function () {
+    Artisan::call('optimize:clear');
+    return ' Cache cleared successfully!';
+});
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage link created successfully!';
+});
+
 Route::get('/blog-post-image-generator', [ImageGeneratorController::class, 'index'])->name('image.index');
 require __DIR__.'/auth.php';
 require __DIR__.'/website.php';
